d92f864a430d532ab4801229ef9de1a5df471a1e
matt2168 <matthew.alsop@dci-student.org>
2025-02-17 16:12:38 +0100

    logging and logging middlewares update

--- crosschecker/checker/views.py
+++ crosschecker/checker/views.py
@@ -6,9 +6,3 @@ from django.http import HttpResponse
 
 logger = logging.getLogger("checker")
 
-#Used as test only if not working, otherwise delete
-'''def my_view(request):
-    logger.info('This is an info message')
-    logger.warning('This is a warning message')
-    logger.error('This is an error message')
-    return HttpResponse('Hello, world!')'''
